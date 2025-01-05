19a4d9dc633b859ebd168c9100ba03364b630718
mayu.watanabe <m_popo.xx@i.softbank.jp>
2025-01-05 16:26:16 +0900

    課題です。

--- src/main/java/com/techacademy/KadaiFirstController.java
+++ src/main/java/com/techacademy/KadaiFirstController.java
@@ -79,19 +79,19 @@ public class KadaiFirstController {
     @GetMapping("/minus/{val1}/{val2}")
     public String calcMinus(@PathVariable int val1,@PathVariable int val2) {
         int res = 0;
-        res = val1 + val2;
+        res = val1 - val2;
         return "計算結果:" + res;
     }
     @GetMapping("/times/{val1}/{val2}")
     public String calcTimes(@PathVariable int val1,@PathVariable int val2) {
         int res = 0;
-        res = val1 + val2;
+        res = val1 * val2;
         return "計算結果:" + res;
     }
     @GetMapping("/divide/{val1}/{val2}")
     public String calcDivide(@PathVariable int val1,@PathVariable int val2) {
         int res = 0;
-        res = val1 + val2;
+        res = val1 / val2;
         return "計算結果:" + res;
     }
 }
